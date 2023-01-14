f406913cf562f2975fac76e21fe2bccab7c3ea29
danyil <danilomaster2003@ukr.net>
2023-01-14 19:59:42 +0200

    feat: add different routes on click

--- client/src/components/welcomePage.tsx
+++ client/src/components/welcomePage.tsx
@@ -1,6 +1,7 @@
 import { Button } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import Background from '../assets/background.png'
+import { isAuthenticated } from '../utils';
 
 export const WelcomePage = () => {
     const navigate = useNavigate();
@@ -8,10 +9,13 @@ export const WelcomePage = () => {
     return (
         <>
             <div style={{ display: "flex", alignItems: "center", justifyContent: "center", height: "70vh" }} className="app-container">
-                <div style={{width: "450px"}}>
+                <div style={{ width: "450px" }}>
                     <h1>Welcome to the University DB</h1>
                     <p style={{ fontSize: "24px" }}>Using our system, you can manage your university with ease! Add and remove students, professors, subjects, groups</p>
-                    <Button onClick={() => navigate("/login")} size="lg">Get started</Button>
+                    <Button onClick={() => {
+                        isAuthenticated() ? navigate("/user") : navigate("/login");
+                    }
+                    } size="lg">Get started</Button>
                 </div>
                 <img src={Background} alt="" width="600px" />
             </div>
